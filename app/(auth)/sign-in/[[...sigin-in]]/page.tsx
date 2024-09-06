d364d51a17bfbd9f0f56d867d5ae99268dccb2bd
Ayush-Petwal <ayushpetwal003@gmail.com>
2024-09-06 12:56:42 +0530

    Lets Meet Start

--- app/(auth)/sign-in/[[...sigin-in]]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { SignIn } from '@clerk/nextjs';
-
-export default function SiginInPage() {
-  return (
-    <main className="flex h-screen w-full items-center justify-center">
-      <SignIn />
-    </main>
-  );
-}
