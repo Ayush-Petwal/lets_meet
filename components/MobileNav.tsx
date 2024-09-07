63a26f233149a72e996c3d13b89852b68bc7542d
Ayush-Petwal <ayushpetwal003@gmail.com>
2024-09-07 14:01:17 +0530

    Fix apostrophe in Let's Meet text

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -35,7 +35,7 @@ const MobileNav = () => {
                     alt=" Let&apos;s Meet "
                     className='max-sm:size-10'
                     />
-                    <p className='text-[26px] font-extrabold text-white'>Let's Meet</p>
+                    <p className='text-[26px] font-extrabold text-white'>Let&apos;s Meet</p>
                 </Link>
 
                 <div className='flex h-[calc(100vh-72px)] flex-col justify-between overflow-y-auto '>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -15,7 +15,7 @@ const Navbar = () => {
           alt=" Let&apos;s Meet "
           className='max-sm:size-10'
         />
-        <p className='text-[26px] font-extrabold text-white max-sm:hidden '>Let's Meet</p>
+        <p className='text-[26px] font-extrabold text-white max-sm:hidden '>Let&apos;s Meet</p>
       </Link>
 
       <div className='flex justify-between items-center gap-5 '> 
